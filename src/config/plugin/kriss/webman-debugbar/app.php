1f3bd05c1bba04df678e0f3b40974b8af6f57161
kriss <462679766@qq.com>
2022-07-15 21:24:47 +0800

    feature: 更新 config

--- src/config/plugin/kriss/webman-debugbar/app.php
+++ src/config/plugin/kriss/webman-debugbar/app.php
@@ -8,13 +8,15 @@ return [
      * @see \Kriss\WebmanDebugBar\WebmanDebugBar::$config
      */
     'debugbar' => [
-        'enable' => $enable,
-        'collectors' => function() {
-            return [];
-        },
+        'enable' => false,
+        'collectors' => null,
         'http_driver' => true,
         'storage' => true,
-        'open_handler_url' => 'debugbar',
-        'asset_base_url' => '/_debugbar',
+        'open_handler_url' => '/_debugbar/open',
+        'asset_base_url' => '/_debugbar/assets',
+        'sample_url' => '/_debugbar/sample',
+        'javascript_renderer_options' => [],
+        'skip_request_path' => ['/_debugbar/open', '/_debugbar/assets/*', '*.css', '*.js'],
+        'skip_request_callback' => null,
     ],
 ];
